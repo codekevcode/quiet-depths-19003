d074a91c58bdfd7adb632f7f5353da3712bb648b
codekevcode <newtonkevin2010@gmail.com>
2017-08-19 17:49:23 -0400

    renaming files to make sense

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import './App.css';
 import App from './App';
 import Feature from './feature';
-import { AttentionGrabber } from './fold';
+import { SurveyBar } from './SurveyBar';
 import { Nav } from './nav';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Nav />, document.getElementById('menu'));
 ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Feature />, document.getElementById('feature'));
-ReactDOM.render(<AttentionGrabber />, document.getElementById('top'));
+ReactDOM.render(<SurveyBar />, document.getElementById('surveybar'));
 registerServiceWorker();
