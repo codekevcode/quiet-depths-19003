1f7558c01897501bcd71851868c9d65a623379e1
codekevcode <newtonkevin2010@gmail.com>
2017-08-22 23:55:06 -0400

    Consolidated SurveyButton into SurveyBanner.js

--- src/SurveyBanner.js
+++ src/SurveyBanner.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import './App.css';
-import { SurveyButton } from './SurveyButton';
 
 
+
+class SButton extends React.Component {
+	render() {
+		return (
+
+          <div className="col-4 ">
+            <p><a className="btn sb btn-primary btn-lg btn-block" href="http://www.google.com" target="_blank"> Take Survey</a></p>
+          </div>
+    );
+	}
+}
 
 export class SurveyBanner extends React.Component {
 	render() {
@@ -12,10 +22,11 @@ export class SurveyBanner extends React.Component {
           <div className="col-8 align-baseline">
         <h3>  We are doing a survey about the usage of our bot and we want to know what you think </h3>
           </div>
-          <SurveyButton />
+          <SButton />
         </div>
       </div>
     );
 	}
 }
+
 export default SurveyBanner	;
